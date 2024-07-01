03d32de9b86095d309d7b3cdfe6012425794b81b
OmPrakashSahoo-05 <ommprcash@gmail.com>
2024-07-01 07:18:33 +0530

    latest feature

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,14 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
@@ -16,7 +24,16 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatCardModule,
+    MatButtonModule,
+    MatInputModule,
+    MatIconModule,
+    MatFormFieldModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
-
-  constructor() { }
+myform!:FormGroup
+  constructor( private fb:FormBuilder, private http:HttpClient,private router:Router) { }
 
   ngOnInit(): void {
+    this.myform=this.fb.group({
+      name:['', Validators.required],
+      email:['',Validators.required],
+      mobile:['',Validators.required],
+      password:['',Validators.required],
+    });
   }
-
+  signup(){
+    this.http.post<any>("http://localhost:3000/signup",this.myform.value).subscribe(res=>
+      {
+        alert("You are registered successfully!");
+        this.myform.reset();
+        this.router.navigate(['login']);
+      },error=>{
+        alert("Something went wrong!!!");
+      }
+    )
+  }
 }
